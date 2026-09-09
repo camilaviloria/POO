5e526cb30340353925792f10f310c701ef3b85c1
camilaviloria <164042700+camilaviloria@users.noreply.github.com>
2026-09-09 14:25:20 -0400

    feat: se agregaron patrones hechos en clases

---
+++ patrones/decorator/doll.java
@@ -0,0 +1,6 @@
+package patrones.decorator;
+//clase o interfaz abstracta que define el comportamiento que puede ser alterado por los decoradores
+public interface doll {
+    public String scare();
+    public String speak();
+}
